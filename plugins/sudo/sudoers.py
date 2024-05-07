eb5f8b73d6260fe460b24943ef44db5f1c4bae7d
Molly Mitchell <mmitc103@asu.edu>
2024-05-07 21:17:32 +0530

    Add files via upload

---
+++ plugins/sudo/sudoers.py
@@ -0,0 +1,75 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from NOAHxMusic import app
+from NOAHxMusic.misc import SUDOERS
+from NOAHxMusic.utils.database import add_sudo, remove_sudo
+from NOAHxMusic.utils.decorators.language import language
+from NOAHxMusic.utils.extraction import extract_user
+from NOAHxMusic.utils.inline import close_markup
+from config import BANNED_USERS, OWNER_ID
+
+
+@app.on_message(filters.command(["addsudo"]) & filters.user(OWNER_ID))
+@language
+async def useradd(client, message: Message, _):
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+            return await message.reply_text(_["general_1"])
+    user = await extract_user(message)
+    if user.id in SUDOERS:
+        return await message.reply_text(_["sudo_1"].format(user.mention))
+    added = await add_sudo(user.id)
+    if added:
+        SUDOERS.add(user.id)
+        await message.reply_text(_["sudo_2"].format(user.mention))
+    else:
+        await message.reply_text(_["sudo_8"])
+
+
+@app.on_message(filters.command(["delsudo", "rmsudo"]) & filters.user(OWNER_ID))
+@language
+async def userdel(client, message: Message, _):
+    if not message.reply_to_message:
+        if len(message.command) != 2:
+            return await message.reply_text(_["general_1"])
+    user = await extract_user(message)
+    if user.id not in SUDOERS:
+        return await message.reply_text(_["sudo_3"].format(user.mention))
+    removed = await remove_sudo(user.id)
+    if removed:
+        SUDOERS.remove(user.id)
+        await message.reply_text(_["sudo_4"].format(user.mention))
+    else:
+        await message.reply_text(_["sudo_8"])
+
+
+@app.on_message(filters.command(["sudolist", "listsudo", "sudoers"]) & ~BANNED_USERS)
+@language
+async def sudoers_list(client, message: Message, _):
+    if message.from_user.id not in SUDOERS:
+        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/NEETxNOAH'>🇷🇺 N O A H 🇳</a>",
+        disable_web_page_preview=True,
+        parse_mode="html")
+    text = _["sudo_5"]
+    user = await app.get_users(OWNER_ID)
+    user = user.first_name if not user.mention else user.mention
+    text += f"1➤ {user}\n"
+    count = 0
+    smex = 0
+    for user_id in SUDOERS:
+        if user_id != OWNER_ID:
+            try:
+                user = await app.get_users(user_id)
+                user = user.first_name if not user.mention else user.mention
+                if smex == 0:
+                    smex += 1
+                    text += _["sudo_6"]
+                count += 1
+                text += f"{count}➤ {user}\n"
+            except:
+                continue
+    if not text:
+        await message.reply_text(_["sudo_7"])
+    else:
+        await message.reply_text(text, reply_markup=close_markup(_))
---
+++ plugins/tools/alive.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from NOAHxMusic import app
+from pyrogram import filters
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from config import MUSIC_BOT_NAME
+
+@app.on_message(filters.command(["alive"]))
+async def start(client: Client, message: Message):
+    await message.reply_video(
+        video=f"https://te.legra.ph/file/9c279179b258b3bb9902f.mp4",
+        caption=f"❤️ ʜᴇʏ {message.from_user.mention}\n\n🔮 ɪ ᴀᴍ {MUSIC_BOT_NAME}\n\n✨ ɪ ᴀᴍ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.\n\n💫 ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴛʜᴇɴ ᴊᴏɪɴ ᴏᴜʀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ🤍...\n\n━━━━━━━━━━━━━━━━━━❄",
+        reply_markup=InlineKeyboardMarkup(
+            [
+               [
+            InlineKeyboardButton(
+                text="☆ OWNER 💗 ", url=f"https://t.me/NEETxNOAH"
+            ),
+            InlineKeyboardButton(
+                text="☆ SUPPORT 💗", url=f"https://t.me/neetxdiscussion"
+            ),
+        ],
+                [
+            InlineKeyboardButton(
+                text="☆ CHANNEL 💗", url=f"https://t.me/aboutNEETxNOAH"
+            ),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "✯ ᴄʟᴏsᴇ ✯", callback_data="close"
+                    )
+                ],
+            ]
+        )
+    )
